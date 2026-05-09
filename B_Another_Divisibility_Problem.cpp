59a6413f87f5a530a6edbf06646175bac6ad0a45
swarn <swarn@swarns-MacBook-Air.local>
2026-05-09 22:31:24 +0530

    cpian3

---
+++ B_Another_Divisibility_Problem.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+using namespace std;
+int main(){
+    int t; cin >> t;
+    while(t--) {
+        long long x; cin >> x;
+        cout << 2*x << "\n";
+    }
+}
